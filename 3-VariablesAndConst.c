1a69a0c53d3e11077d16e1a3137c6bac997c6649
ercelserhat <ercelserhat@gmail.com>
2024-01-29 16:22:26 +0300

    Variables

---
+++ 3-VariablesAndConst.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	char karakter = 's';
+	char *isim = "Serhat";
+	int sayi = 23;
+	float f = 33.3;
+	double d = 44.4;
+	
+	printf("Karakter = %c\n", karakter);
+	printf("Isim = %s\n", isim);
+	printf("Integer = %d\n", sayi);
+	printf("Float = %f\n", f);
+	printf("Double = %lf\n", d);
+	
+	return 0;
+	
+}
