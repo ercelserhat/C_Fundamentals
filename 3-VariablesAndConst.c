73b698953d532aa6b5933057f396d43afdc1e714
ercelserhat <ercelserhat@gmail.com>
2024-01-29 16:31:40 +0300

    Consts

--- 3-VariablesAndConst.c
+++ 3-VariablesAndConst.c
@@ -14,6 +14,22 @@ int main()
 	printf("Float = %f\n", f);
 	printf("Double = %lf\n", d);
 	
+	
+	/* CONSTS */
+	const float PI = 3.142857;
+	const double NOT = 12345.67890;
+	const int NEGATIF = -1;
+	const *METIN = "Devam etmek icin bir tusa basin...";
+	
+	#define MAX 100
+	#define DATA 0x0378
+	#define YARICAP 14.22
+	
+	printf("PI: %f\n", PI);
+	printf("NOT: %lf\n", NOT);
+	printf("NEGATIF: %d\n", NEGATIF);
+	printf("METIN: %s\n", METIN);
+	
 	return 0;
 	
 }
