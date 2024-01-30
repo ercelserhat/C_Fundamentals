af810c675d625585d4d39462c4b365add7388538
ercelserhat <ercelserhat@gmail.com>
2024-01-30 12:52:05 +0300

    Loops: Do While

--- 7-Loops.c
+++ 7-Loops.c
@@ -10,5 +10,15 @@ int main()
 		printf("%d\n", x++);
 	}
 	
+	
+	/* DO-WHILE LOOP */
+	int sayi;
+	do
+	{
+		printf("Bir sayi girin: ");
+		scanf("%d", &sayi);
+		printf("Iki kati : %d\n", 2*sayi);
+	}while(sayi > 0);
+	
 	return 0;
 }
