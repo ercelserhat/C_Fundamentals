4a34db0e52786e162b3a70b84782d4be57df5924
ercelserhat <ercelserhat@gmail.com>
2024-01-30 14:24:41 +0300

    Loops: For

--- 7-Loops.c
+++ 7-Loops.c
@@ -20,5 +20,18 @@ int main()
 		printf("Iki kati : %d\n", 2*sayi);
 	}while(sayi > 0);
 	
+	
+	/* FOR LOOP */
+	long i, n, faktor;
+	printf("Faktoriyeli hesaplanacak sayi girin: ");
+	scanf("%ld", &n);
+	
+	faktor = 1;
+	for(i = 1; i <= n; i++)
+	{
+		faktor *= i;
+	}
+	printf("%ld! = %ld\n", n, faktor);
+	
 	return 0;
 }
