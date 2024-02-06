0f7980778c8b0d3236cd5cc3f6083a08ba4e2a18
ercelserhat <ercelserhat@gmail.com>
2024-02-06 14:07:52 +0300

    Preprocessors: If, Elif, Else, Endif

---
+++ 28-Preprocessors-2.c
@@ -0,0 +1,21 @@
+/*PREPROCESSORS
+#if, #elif, #else, #endif
+*/
+
+
+#include <stdio.h>
+
+#if _WIN32
+	#define ISLETIM_SISTEMI "WINDOW ISLETIM SISTEMI"
+#elif __linux__
+	#define ISLETIM_SISTEMI "LINUX ISLETIM SISTEMI"
+#elif __APPLE__
+	#define ISLETIM_SISTEMI "MAC ISLETIM SISTEMI"
+#else
+	#define ISLETIM_SISTEMI "X ISLETIM SISTEMI"
+#endif
+
+int main()
+{
+	printf(ISLETIM_SISTEMI);
+}
